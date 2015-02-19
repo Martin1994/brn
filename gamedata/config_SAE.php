43636c9ed8cfc1c861e956a87c2a729dbdbde770
Martin Chloride <i@martincl2.me>
2015-02-18 21:17:45 -0500

    Update config file for SAE

--- gamedata/config_SAE.php
+++ gamedata/config_SAE.php
@@ -8,6 +8,7 @@ define('MOD_NAME', 'thbr');
 
 //Lock Configuration
 define('FILE_LOCK', false);
+define('FILE_LOCK_PATH', ROOT_DIR.'/brn.lock');
 
 //Cookie Configuration
 define('COOKIE_PREFIX', 'ACBR_');
@@ -26,10 +27,10 @@ $CACHE_CONFIG = array(
 	);
 
 //Database Configuration
-define('DB_TYPE', 'mysql'); //Available: mysql mongodb pdo_mysql
+define('DB_TYPE', 'pdo_mysql'); //Available: mysql mongodb pdo_mysql
 define('DB_HOST_M', SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT);
 define('DB_HOST_S', false);
-define('DB_PERSISTENT', true);
+define('DB_PERSISTENT', false);
 define('DB_USER', SAE_MYSQL_USER);
 define('DB_PASS', SAE_MYSQL_PASS);
 define('DB_NAME', SAE_MYSQL_DB);
@@ -38,7 +39,7 @@ define('DB_TABLE_PREFIX', 'brn_');
 //Comet Configuration
 define('COMET_SLEEP', 250000); //Microseconds
 define('COMET_TIMEOUT', 25); //Seconds
-define('COMET_TYPE', 'channel_SAE'); //Available: file mongodb memcache memcache_SAE channel_SAE
+define('COMET_TYPE', 'memcache_SAE'); //Available: file mongodb memcache memcache_SAE channel_SAE chlorocomet
 $COMET_CONFIG = array(
 	'file' => array(
 		'dir' => 'cache/comet'
@@ -58,8 +59,14 @@ $COMET_CONFIG = array(
 	'channel_SAE' => array(
 		'expired_time' => 21600,
 		'cache_prefix' => 'channel/'
-		)
-	);
+		),
+	'chlorocomet' => array(
+		'host' => 'http://localhost:85',
+		'client' => 'http://localhost:85',
+		'id' => 'test',
+		'pass' => 'test',
+		'cache_prefix' => 'chlorocomet/'
+	));
 
 //Template Configuration
 define('TEMPLATE_DIR', ROOT_DIR.'/template');
